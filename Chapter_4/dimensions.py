20631585c3a8c5c0dfe53b28172a94b68198600f
PDob02 <patrickdobry@gmail.com>
2022-05-12 15:57:42 -0500

    More exercises

---
+++ Chapter_4/dimensions.py
@@ -0,0 +1,9 @@
+dimensions = (200, 50)
+# dimensions[0] = 250
+print(dimensions[0])
+print(dimensions[1])
+
+for dimension in dimensions:
+    print(dimension)
+
+print("Original dimensions:")
---
+++ Chapter_4/foods.py
@@ -0,0 +1,18 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+
+#This does not work:
+friend_foods = my_foods
+
+friend_foods = my_foods[:]
+
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+
+print("My favorite foods are:")
+print(my_foods)
+
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
+
+print("\nMy friend's favorite foods are:")
+print(friend_foods)

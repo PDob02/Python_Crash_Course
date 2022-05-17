a5334ed70871bad6dfe2f94d63fdb8a04b2ea046
Patrick Dobry <patrickdobry@gmail.com>
2022-05-16 22:02:38 -0500

    Monday night work

---
+++ Chapter_5/amusement_park.py
@@ -0,0 +1,17 @@
+age = 12
+if age < 4:
+    print("Your admission cost is $0.")
+elif age < 18:
+    print("Your admission cost is $25.")
+else:
+    print("Your admission cost is $40.")
+
+age = 21
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+else:
+    price = 40
+
+print(f"Your admission cost is ${price}.")

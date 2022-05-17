ca12301434877ca62757cf01a2686dfa1000de8e
PDob02 <patrickdobry@gmail.com>
2022-05-16 21:32:59 -0500

    Adding chapter 5

--- Chapter_4/dimensions.py
+++ Chapter_4/dimensions.py
@@ -7,3 +7,10 @@ for dimension in dimensions:
     print(dimension)
 
 print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 100)
+print("\nModified dimensions:")
+for dimension in dimensions:
+    print(dimension)
---
+++ Chapter_5/cars.py
@@ -0,0 +1,9 @@
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+# car == 'bmw'
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())

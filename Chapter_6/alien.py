ddce0899751a9d54aca691f04e46d1c323f2660e
PDob02 <patrickdobry@gmail.com>
2022-05-18 18:46:05 -0500

    Wednesday night push

--- Chapter_6/alien.py
+++ Chapter_6/alien.py
@@ -8,3 +8,14 @@ print(f"You just earned {new_points} points!")
 alien_0['x_position'] = 0
 alien_0['y_position'] = 25
 print(alien_0)
+
+alien_0 = {}
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+print(alien_0)
+
+alien_0 = {'color' : 'green'}
+print(f"The alien is {alien_0['color']}.")
+
+alien_0['color'] = 'yellow'
+print(f"The alien is now {alien_0['color']}.")

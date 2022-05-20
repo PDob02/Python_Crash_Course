4e3d61350cb283b3f37e3375253a5ebaa9b142a7
PDob02 <patrickdobry@gmail.com>
2022-05-20 12:59:02 -0500

    Friday work

--- Chapter_6/alien.py
+++ Chapter_6/alien.py
@@ -19,3 +19,22 @@ print(f"The alien is {alien_0['color']}.")
 
 alien_0['color'] = 'yellow'
 print(f"The alien is now {alien_0['color']}.")
+
+
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'fast'}
+print(f"Original position: {alien_0['x_position']}")
+
+# Move the alien to the right
+# Determine how far to move the alien based on its current speed
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    # this must be a fast alien
+    x_increment = 3
+
+# The new position is the old position plus the increment
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+
+print(f"New position: {alien_0['x_position']}")

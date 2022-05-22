aa5433f4f5671dbf2dc7565528152bb5ff3741a4
PDob02 <patrickdobry@gmail.com>
2022-05-21 19:13:13 -0500

    Adding Saturday night work

--- Chapter_6/favorite_languages.py
+++ Chapter_6/favorite_languages.py
@@ -7,3 +7,22 @@ favorite_languages = {
 
 language = favorite_languages['sarah'].title()
 print(f"Sarah's favorite language is {language}.")
+
+for name, language in favorite_languages.items():
+    print(f"{name.title()}'s favorite language is {language.title()}.")
+
+for name in favorite_languages.keys():
+    print(name.title())
+
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(f"Hi {name.title()}.")
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}!")
+
+if 'erin' not in favorite_languages.keys():
+    print("Erin, please take our poll!")
+
+for name in sorted(favorite_languages.keys()):
+    print(f"{name.title()}, thank you for taking the poll.")

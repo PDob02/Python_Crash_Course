1ad3b4b67bccdcb6109727d6d2619fcbd00097c9
PDob02 <patrickdobry@gmail.com>
2022-05-22 17:29:51 -0500

    Adding more work sunday

--- Chapter_7/parrot.py
+++ Chapter_7/parrot.py
@@ -1,2 +1,11 @@
 message = input("Tell me something, and I will repeate it back to you: ")
 print(message)
+
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit to end the program."
+message = ""
+while message != 'quit':
+    message = input(prompt)
+
+    if message != 'quit':
+        print(message)

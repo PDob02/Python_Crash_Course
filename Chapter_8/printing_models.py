fcdbe70b722c39a5b160baa8bfd6b7669cf9ac9c
PDob02 <patrickdobry@gmail.com>
2022-06-03 10:32:55 -0500

    Adding more files

--- Chapter_8/printing_models.py
+++ Chapter_8/printing_models.py
@@ -1,15 +1,37 @@
-# Start with some designs that need to be printed
+# # Start with some designs that need to be printed
+# unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+# completed_models = []
+
+# #Simulate printing each design, until none are left
+# #Move each design to completed_models after printing
+# while unprinted_designs:
+#     current_design = unprinted_designs.pop()
+#     print(f"Printing model: {current_design}")
+#     completed_models.append(current_design)
+    
+# #Display all completed models
+# print("\nThe following models have been printed:")
+# for completed_model in completed_models:
+#     print(completed_model)
+
+def print_models(unprinted_designs, completed_models):
+    """
+    Simulate printing each design, until none are left
+    move each design to completed_models after printing
+    """
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        print(f"Printing model: {current_design}")
+        completed_models.append(current_design)
+
+def show_completed_models(completed_models):
+    """SHow all the models that were printed."""
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
+
 unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
 completed_models = []
 
-#Simulate printing each design, until none are left
-#Move each design to completed_models after printing
-while unprinted_designs:
-    current_design = unprinted_designs.pop()
-    print(f"Printing model: {current_design}")
-    completed_models.append(current_design)
-    
-#Display all completed models
-print("\nThe following models have been printed:")
-for completed_model in completed_models:
-    print(completed_model)
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)

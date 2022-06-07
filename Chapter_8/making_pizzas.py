91c574a3cc4f8578c351cba44f75c672133a9a47
PDob02 <patrickdobry@gmail.com>
2022-06-07 15:27:16 -0500

    Starting Chapter 9

---
+++ Chapter_8/making_pizzas.py
@@ -0,0 +1,18 @@
+#practicing dot notation
+# import pizza
+# from pizza import make_pizza
+# from pizza import make_pizza as mp
+# import pizza as p
+from pizza import *
+
+# pizza.make_pizza(16, 'pepperoni')
+# pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra chese')
+
+# mp(16, 'pepperoni')
+# mp(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+# p.make_pizza(16, 'pepperoni')
+# p.make_pizza(12, 'mushrooms', 'green peppers', 'extra chese')
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra chese')
---
+++ Chapter_9/dog.py
@@ -0,0 +1,15 @@
+class Dog:
+    """A simple attempt to model a dog."""
+
+    def _init_(self, name, age):
+        """initialize name and age attributes."""
+        self.name = name
+        self.age = age
+    
+    def sit(self):
+        """Simulate a dog sitting in response to a command."""
+        print(f"{self.name} is now sitting.")
+
+    def roll_over(self):
+        """Simulate rolling over in response to a command."""
+        print(f"{self.name} rolled over!")

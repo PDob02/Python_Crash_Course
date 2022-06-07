43b9e1058a8eedce710068a50ba7b3cb1bcd8804
PDob02 <patrickdobry@gmail.com>
2022-06-07 17:44:50 -0500

    Adding Tuesday work

---
+++ Chapter_9/car.py
@@ -0,0 +1,30 @@
+class Car:
+    """A simple attempt to represent a car."""
+
+    def __init__(self, make, model, year):
+        """Initialize attributes to a describe a car."""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+    
+    def get_descriptive_name(self):
+        """Return a neatly formatted descriptive name."""
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometer(self):
+        """Print a statement showing the car's mileage"""
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage):
+        """Set the odometer reading to the given value."""
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+my_new_car = Car('audi', 'a4', 2019)
+print(my_new_car.get_descriptive_name())
+my_new_car.odometer_reading = 24
+my_new_car.read_odometer()
--- Chapter_9/dog.py
+++ Chapter_9/dog.py
@@ -1,7 +1,7 @@
 class Dog:
     """A simple attempt to model a dog."""
 
-    def _init_(self, name, age):
+    def __init__(self, name, age):
         """initialize name and age attributes."""
         self.name = name
         self.age = age
@@ -13,3 +13,14 @@ class Dog:
     def roll_over(self):
         """Simulate rolling over in response to a command."""
         print(f"{self.name} rolled over!")
+
+my_dog = Dog('Willie', 6)
+your_dog = Dog('Lucy', 3)
+
+print(f"My dog's name is {my_dog.name}.")
+print(f"My dog is {my_dog.age} years old.")
+my_dog.sit()
+
+print(f"My dog's name is {your_dog.name}.")
+print(f"My dog is {your_dog.age} years old.")
+your_dog.sit()

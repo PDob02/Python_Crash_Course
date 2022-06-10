98b41227ffc39d63ab337e2fce2968f8360bbc1d
PDob02 <patrickdobry@gmail.com>
2022-06-10 12:39:36 -0500

    Latest chagnes

--- Chapter_9/car.py
+++ Chapter_9/car.py
@@ -27,15 +27,44 @@ class Car:
     def increment_odometer(self, miles):
         """Add the given amount to th odometer reading"""
         self.odometer_reading += miles
+class Battery:
+    """A simple attempt to model a batter for an electric car"""
+    
+    def __init__(self, battery_size = 75):
+        """Intialize the battery's attributes"""
+        self.battery_size = battery_size
+    
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+    
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
+        print(f"This car can go about {range} miles on a full charge.")
+        
+class ElectricCar(Car):
+    """Represents aspects of a car, specific to electric vehicles"""
 
-my_used_car = Car('subaru', 'outback', 2015)
-print(my_used_car.get_descriptive_name())
+    def __init__(self, make, model, year):
+        """Initialize attributes of the parent class.
+        Initialize attributes of the parent class.
+        Then intialize attributes specific to an electric car.
+        """
+        super().__init__(make, model, year)
+        self.battery = Battery()
+    
+# my_used_car = Car('subaru', 'outback', 2015)
+# print(my_used_car.get_descriptive_name())
 
-my_used_car.update_odometer(23_500)
-my_used_car.read_odometer()
+# my_used_car.update_odometer(23_500)
+# my_used_car.read_odometer()
 
-my_used_car.increment_odometer(100)
-my_used_car.read_odometer()
+# my_used_car.increment_odometer(100)
+# my_used_car.read_odometer()
 
 # my_new_car = Car('audi', 'a4', 2019)
 # print(my_new_car.get_descriptive_name())
--- Chapter_9/electric_car.py
+++ Chapter_9/electric_car.py
@@ -30,6 +30,24 @@ class Car:
     def increment_odometer(self, miles):
         """Add the given amount to th odometer reading"""
         self.odometer_reading += miles
+class Battery:
+    """A simple attempt to model a batter for an electric car"""
+    
+    def __init__(self, battery_size = 75):
+        """Intialize the battery's attributes"""
+        self.battery_size = battery_size
+    
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+    
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
+        print(f"This car can go about {range} miles on a full charge.")
 
 class ElectricCar(Car):
     """Represents aspects of a car, specific to electric vehicles"""
@@ -40,16 +58,19 @@ class ElectricCar(Car):
         Then intialize attributes specific to an electric car.
         """
         super().__init__(make, model, year)
-        self.battery_size = 75
+        self.battery = Battery()
     
-    def describe_battery(self):
-        """Print a statement describing the battery size."""
-        print(f"This car has a {self.battery_size}-kWh battery.")
+    # def describe_battery(self):
+    #     """Print a statement describing the battery size."""
+    #     print(f"This car has a {self.battery_size}-kWh battery.")
 
     def fill_gas_tank(self):
         """Electric cars don't have gas tanks."""
         print("This car doesn't need a gas tank!")
 
 my_tesla = ElectricCar('tesla', 'model s', 2019)
+
 print(my_tesla.get_descriptive_name())
-my_tesla.describe_battery()
+# my_tesla.describe_battery()
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()

59452a099e20080ac9850d0f23bf98622fa9d3c3
PDob02 <patrickdobry@gmail.com>
2022-06-10 13:40:10 -0500

    Adding files

---
+++ Chapter_10/file_reader.py
@@ -0,0 +1,9 @@
+filename = 'pi_digits.txt'
+
+with open(filename) as file_object:
+    for line in file_object:
+        print(line.rstrip())
+
+with open('pi_digits.txt') as file_object:
+    contents = file_object.read()
+print(contents.rstrip())
---
+++ Chapter_9/choice.py
@@ -0,0 +1,4 @@
+from random import choice
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+first_up = choice(players)
+print(first_up)

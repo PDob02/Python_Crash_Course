e544cfd6d4617fc947e4539f35184c444d53f476
PDob02 <patrickdobry@gmail.com>
2022-06-15 18:53:17 -0500

    Adding new files

--- Chapter_11/test_name_function.py
+++ Chapter_11/test_name_function.py
@@ -1,4 +1,5 @@
 import unittest
+
 from name_function import get_formatted_name
 
 class NamesTestCase(unittest.TestCase):
@@ -8,6 +9,13 @@ class NamesTestCase(unittest.TestCase):
         """Do names like 'Janis Joplin' work?"""
         formatted_name = get_formatted_name('janis','joplin')
         self.assertEqual(formatted_name, 'Janis Joplin')
+
+    def test_first_last_middle_name(self):
+        """Do names like 'Wolfgang Amadeus Mozart' work?"""
+        formatted_name = get_formatted_name(
+        'wolfgang', 'mozart', 'amadeus')
+        self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')
+
 
 if __name__ == '__main__':
     unittest.main()
